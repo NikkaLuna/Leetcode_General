e0fcbba4c765129deeda9aed37dacd712735e9c2
Andrea Hayes <ahaye74@wgu.edu>
2024-06-11 12:37:27 -0500

    Add Solution 118. Pascal's Triangle

--- 118.Pascal's Triangle/Solution.java
+++ 118.Pascal's Triangle/Solution.java
@@ -1,30 +1,5 @@
 
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        
-        // Start with the first string as the initial prefix
-        String prefix = strs[0];
-        
-        for (int i = 1; i < strs.length; i++) {
-
-            // Update the prefix by comparing it with the current string
-            while (strs[i].indexOf(prefix) != 0) {
-                
-                // Reduce the prefix length by one until it matches the beginning of the current string
-                prefix = prefix.substring(0, prefix.length() - 1);
-
-                // If the prefix becomes empty, return ""
-                if (prefix.isEmpty()) return "";
-            }
-        }
-        
-        return prefix;
-    }
-}
-
-
-public class Solution {
+public public class Solution {
     
     public List<List<Integer>> generate(int numRows) {
         
@@ -53,3 +28,4 @@ public class Solution {
 }
 
     
+ 
